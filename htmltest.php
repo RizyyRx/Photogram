240a72a55bd7ea05cfba5499958c04046fb457a8
Rizwanudeen K <rizwankendo@gmail.com>
2024-03-31 19:14:41 +0000

    Added a login page

---
+++ htmltest.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>
+        this is an image
+        </h1>
+        <img src="/app/maeve 3.png" alt="imagehere" width="72" height="57">
+    </body>
+</html>
--- libs/load.php
+++ libs/load.php
@@ -2,7 +2,7 @@
 /*loads a template file if called.....
 file name should be given as a parameter*/
 function load_template($name){
-    print("including ".$_SERVER['DOCUMENT_ROOT']." $name.php");
+    //print("including ".$_SERVER['DOCUMENT_ROOT']." $name.php");
     include $_SERVER['DOCUMENT_ROOT']."/app/_templates/$name.php";
 }
 ?>
