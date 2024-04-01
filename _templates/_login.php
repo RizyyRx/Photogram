11702cc7cf0ed09e7bbf035a06afa56c603ae79c
Rizwanudeen K <rizwankendo@gmail.com>
2024-04-01 06:06:34 +0000

    basic login implementation done with static credentials

--- _templates/_login.php
+++ _templates/_login.php
@@ -1,14 +1,34 @@
+<?php
+$username=$_POST['email_addr'];
+$password=$_POST['pass'];
+$result=credential_validation($username,$password);
+if($result){
+?>
+<main class="container">
+  <div class="bg-body-tertiary p-5 rounded mt-3">
+    <h1>Login successful</h1>
+    <p class="lead">This example to understand how login works with html forms</p>
+    <a class="btn btn-lg btn-primary" href="/docs/5.3/components/navbar/" role="button">View navbar docs »</a>
+  </div>
+</main>
+<?
+}
+else{
+?>
+
+
+
 <main class="form-signin w-100 m-auto">
-  <form>
+  <form method="post" action="login.php">
     <img class="ms-1 mb-3" src="/app/maeve 3.png" alt="image here" height="150">
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <input name="email_addr" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
       <label for="floatingInput">Email address</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <input name="pass" type="password" class="form-control" id="floatingPassword" placeholder="Password">
       <label for="floatingPassword">Password</label>
     </div>
 
@@ -21,3 +41,6 @@
     <button class="btn btn-primary w-100 py-2 hvr-buzz-out" type="submit">Sign in</button>
   </form>
 </main>
+<?
+}
+?>
--- libs/load.php
+++ libs/load.php
@@ -5,4 +5,13 @@ function load_template($name){
     //print("including ".$_SERVER['DOCUMENT_ROOT']." $name.php");
     include $_SERVER['DOCUMENT_ROOT']."/app/_templates/$name.php";
 }
+
+function credential_validation($username,$password){
+    if($username=="rizwan@gmail.com" and $password=="password"){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 ?>
